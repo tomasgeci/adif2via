16189df45876d285f8bc3b0f3f5b00ac84ad310f
Tomas Geci <tomas.geci@gmail.com>
2022-11-15 15:44:33 +0100

    feat(adif): multiple outputs for adif + partially fixed messy path handling

--- bin/adif2via.js
+++ bin/adif2via.js
@@ -8,13 +8,14 @@ import boxen from 'boxen';
 import {AdifFormatter, AdifParser} from 'adif-parser-ts';
 import dayjs from 'dayjs';
 import {parse} from 'csv-parse/sync';
+import path from 'path'
 import sortPkg from 'fast-sort';
 
 const {sort} = sortPkg;
 
 const cliOptions = yargs(hideBin(process.argv))
     .usage("Usage: -f <file> -m <manager>")
-    .version('1.0.0')
+    .version('1.1.0')
     .option('f', {
         alias: "file",
         describe: "Path to ADIF file ",
@@ -58,41 +59,71 @@ try {
     let adifFileContent = readFileSync(cliOptions.file, 'utf8');
     let adifContent = AdifParser.parseAdi(adifFileContent);
     let decoratedAdifRecords = [];
+    let managerAdifRecords = [];
+    let nonManagerAdifRecords = [];
     let qsoCount = adifContent.records.length;
     let managerCount = managerRecords.length;
     let qsoViaManagerCallsCount = 0;
+    let inputFilePathDir = path.parse(cliOptions.file).dir === '' ? '' : path.parse(cliOptions.file).dir + '/';
+    let inputFilePathFilename = path.parse(cliOptions.file).name;
+    let outputFinishedFilePath = `${inputFilePathDir}finished_${inputFilePathFilename}.adi`;
+    let outputManagerFilePath = `${inputFilePathDir}manager_${inputFilePathFilename}.adi`;
+    let outputNonManagerFilePath = `${inputFilePathDir}non-manager_${inputFilePathFilename}.adi`;
 
     adifContent.records.forEach(qso => {
-        let via = '';
         // TODO: resolve QSL manager date logic
         let manager = managerRecords.find(m => m['call'].toUpperCase() === qso['call'].toUpperCase());
-            if (manager !== undefined) {
-                console.log(
-                    chalk.cyanBright.bold(
-                        qso['call']) + ' VIA ' + chalk.yellow.bold(manager['manager']) +
-                    ' FROM ' + chalk.greenBright.bold(manager['date'])
-                );
-                via = manager['manager'];
-                qsoViaManagerCallsCount++;
-            }
-        qso['qsl_via'] = (qso['qsl_via'] === undefined || qso['qsl_via'] === '') ? via : qso['qsl_via'];
+        if (manager !== undefined) {
+            console.log(
+                `${chalk.cyanBright.bold(qso['call'])} VIA ${chalk.yellow.bold(manager['manager'])} FROM ${chalk.greenBright.bold(manager['date'])}`
+            );
+            qso['qsl_via'] = (qso['qsl_via'] === undefined || qso['qsl_via'] === '') ? manager['manager'] : qso['qsl_via'];
+            managerAdifRecords.push(qso);
+            qsoViaManagerCallsCount++;
+        } else {
+            nonManagerAdifRecords.push(qso);
+        }
         decoratedAdifRecords.push(qso);
     });
 
-    let outputAdif = AdifFormatter.formatAdi({header: adifContent.header, records: decoratedAdifRecords});
-    writeFile('finished_' + cliOptions.file, outputAdif, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-    });
+    writeFile(
+        outputFinishedFilePath,
+        AdifFormatter.formatAdi({header: adifContent.header, records: decoratedAdifRecords}),
+        function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+
+    writeFile(
+        outputManagerFilePath,
+        AdifFormatter.formatAdi({header: adifContent.header, records: managerAdifRecords}),
+        function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+
+    writeFile(
+        outputNonManagerFilePath,
+        AdifFormatter.formatAdi({header: adifContent.header, records: nonManagerAdifRecords}),
+        function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
 
     const qsoResult = chalk.green.bold(`Processed ${cliOptions.file} with ${qsoCount} QSO records`);
     const managerResult = chalk.cyanBright.bold(
         `Processed ${cliOptions.manager} with ${managerCount} managers\nFound ${qsoViaManagerCallsCount} VIA Calls`
     );
+    const outputFiles = chalk.yellowBright.bold(
+        `Output files:\n\n${outputFinishedFilePath}\n${outputManagerFilePath}\n${outputNonManagerFilePath}`
+    );
 
     console.log(boxen(qsoResult, boxenOptions));
     console.log(boxen(managerResult, boxenOptions));
+    console.log(boxen(outputFiles, boxenOptions));
     console.log('Execution time: %dms', new Date() - start);
 
 } catch (err) {
--- readme.md
+++ readme.md
@@ -20,16 +20,18 @@ Database of QSL managers: https://www.okdxf.eu/index.php/databaze-qsl-manazeru
 - download file from `https://www.okdxf.eu/index.php/databaze-qsl-manazeru`
 - unzip and place file to folder with this application - default is `qslmgr.txt`
 - export ADIF file with desired QSO from your LOG
-- run `node /bin/adif2via.js -f your_adif_file.adi -m qslmgr.txt`
-- check for `finished_your_adif_file.adi` file with QSL_VIA based-on `qslmgr.txt` QSL managers information
+- run `node /bin/adif2via.js -f your_path/your_adif_file.adi -m qslmgr.txt`
+- check for output `your_path/finished_your_adif_file.adi` file with QSL_VIA based-on `qslmgr.txt` QSL managers information
+- check for output `your_path/manager_your_adif_file.adi` file **with** QSL_VIA only QSO
+- check for output `your_path/non-manager_your_adif_file.adi` file **without** QSL_VIA only QSO
 
 ## Devel
 
+- tested with nodejs `16.*` on Windows 10
 - update deps `npx npm-check-updates -u`
 
 ## TODO
 
 - resolve logic for one callsign with multiple manager by different date (e.g. use od not use QSO date for match)
 - add switch for QSL_SENT status
-- tests
 - clean-up messy code
